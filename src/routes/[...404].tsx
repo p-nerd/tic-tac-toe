d4019d42f76922f37023848831c376d6c128c0bd
Shihab Mahamud <67628903+p-nerd@users.noreply.github.com>
2024-04-28 15:03:19 +0600

    404 page

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -1,25 +1,16 @@
-import { A } from "@solidjs/router";
-
-export default function NotFound() {
+const NotFound = () => {
     return (
-        <main class="mx-auto p-4 text-center text-gray-700">
-            <h1 class="max-6-xs my-16 text-6xl font-thin uppercase text-sky-700">Not Found</h1>
-            <p class="mt-8">
-                Visit{" "}
-                <a href="https://solidjs.com" target="_blank" class="text-sky-600 hover:underline">
-                    solidjs.com
-                </a>{" "}
-                to learn how to build Solid apps.
-            </p>
-            <p class="my-4">
-                <A href="/" class="text-sky-600 hover:underline">
-                    Home
-                </A>
-                {" - "}
-                <A href="/about" class="text-sky-600 hover:underline">
-                    About Page
-                </A>
-            </p>
+        <main class="flex h-screen flex-col items-center justify-center bg-black-400 text-gray-400">
+            <div class="flex flex-auto flex-col items-center justify-center px-4 text-center sm:flex-row">
+                <h1 class="text-2xl font-extrabold tracking-tight text-slate-900 sm:mr-6 sm:border-r sm:border-slate-900/10 sm:pr-6 sm:text-3xl dark:text-slate-200 sm:dark:border-slate-300/10">
+                    404
+                </h1>
+                <h2 class="mt-2 text-lg text-slate-700 sm:mt-0 dark:text-slate-400">
+                    This page could not be found.
+                </h2>
+            </div>
         </main>
     );
-}
+};
+
+export default NotFound;
