b2bf3103f8510624ed2d60e0ccb37a4d55cc0e91
Shihab Mahamud <67628903+p-nerd@users.noreply.github.com>
2024-04-28 14:57:07 +0600

    inital home page

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                "black-400": "#192a33",
+                "black-300": "#1f3641",
+            },
+        },
+    },
+    plugins: [],
+};
