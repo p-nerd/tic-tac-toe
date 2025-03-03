ae956f7818f62ffc7b2c1e447b3a8c20b85eab8d
Shihab Mahamud <shihab4t@gmail.com>
2025-03-03 19:33:35 +0600

    inject vercel tracking

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 /* @refresh reload */
 import { render } from "solid-js/web";
+import { inject } from "@vercel/analytics";
 
 import "~/main.css";
 import App from "~/App";
+
+inject();
 
 const root = document.getElementById("root");
 
